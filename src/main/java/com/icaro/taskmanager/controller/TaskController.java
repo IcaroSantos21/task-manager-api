fa098381ad5039e7bdacef63b3850295c4765240
Icaro Santos <icaro.santosdev@gmail.com>
2026-07-11 11:01:41 -0300

    fix: change task endpoint from /api/tasks to /tasks

--- src/main/java/com/icaro/taskmanager/controller/TaskController.java
+++ src/main/java/com/icaro/taskmanager/controller/TaskController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/tasks") // Definindo a rota base
+@RequestMapping("/tasks") // Definindo a rota base
 @RequiredArgsConstructor // Criando automaticamente o construtor como final
 public class TaskController {
 
